4fb0d3b583d33c7e0bc81fa8b72dbab81b3a7853
DIVYA ROSE RAJU <divyaroseraju@gmail.com>
2022-10-04 23:00:54 +0100

    Update binary_heap.cpp

--- Data Structures/heap/cpp/binary_heap.cpp
+++ Data Structures/heap/cpp/binary_heap.cpp
@@ -139,21 +139,3 @@ void swap(int *x, int *y)
 	*x = *y; 
 	*y = temp; 
 } 
-
-// Driver program to test above functions 
-int main() 
-{ 
-	MinHeap h(11); 
-	h.insertKey(3); 
-	h.insertKey(2); 
-	h.deleteKey(1); 
-	h.insertKey(15); 
-	h.insertKey(5); 
-	h.insertKey(4); 
-	h.insertKey(45); 
-	cout << h.extractMin() << " "; 
-	cout << h.getMin() << " "; 
-	h.decreaseKey(2, 1); 
-	cout << h.getMin(); 
-	return 0; 
-} 
